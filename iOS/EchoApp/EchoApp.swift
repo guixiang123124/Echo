6ab9c3bf0589a5ab7e842e14d6e19690e301409c
Your Name <xianggui@mac.lan>
2026-02-09 00:52:16 -0800

    Respect cloud sync setting on iOS app launch

--- iOS/EchoApp/EchoApp.swift
+++ iOS/EchoApp/EchoApp.swift
@@ -6,9 +6,12 @@ struct EchoApp: App {
     @StateObject private var authSession = EchoAuthSession.shared
 
     init() {
+        let settings = AppSettings()
         FirebaseBootstrapper.configureIfPossible()
         EchoAuthSession.shared.start()
         CloudSyncService.shared.configureIfNeeded()
+        CloudSyncService.shared.setEnabled(settings.cloudSyncEnabled)
+        CloudSyncService.shared.updateAuthState(user: EchoAuthSession.shared.user)
     }
 
     var body: some Scene {
